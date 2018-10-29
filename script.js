89325fa56a41628e018fa3cf3710f23913c6d385
Farihah Kabir <nadahkabir@gmail.com>
2018-10-29 13:47:42 +0000

    Shopping cart update: add products from cart and calculate total

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var products = [
   {name: "Old Ford Car Model", price: "46", image: "https://s3.amazonaws.com/mernbook/marketplace/Ford.jpg"},
   {name: "Storm Trooper Figurine", price: "23", image: "https://s3.amazonaws.com/mernbook/marketplace/stormtrooper-1995015_960_720.jpg"}
 ];
+
 var cart = localStorage.getItem('cart') 
   ? JSON.parse(localStorage.getItem('cart')) :
   {
@@ -13,19 +14,16 @@ var cart = localStorage.getItem('cart')
     total: 0
   };
 
-localStorage.setItem('cart', JSON.stringify(cart)); //need to stringify or else shows as object,object; not the actual names/values
+localStorage.setItem('cart', JSON.stringify(cart)); //need to stringify or else shows as object; not the actual names/values
 
 $(document).ready(function(){
   products.forEach(function(product,index){ //products page
     var colDiv = $('<div>').addClass('col-md-4');
-    $('#products-row').append(colDiv);
-    
     var cardDiv = $('<div>').addClass('card');//addClass and attr(Attribute) do the same thing
-    colDiv.append(cardDiv);
     
     var productImage = $('<img>').addClass("card-img-top");
-    cardDiv.append(productImage);
     productImage.attr('src', product.image);
+    cardDiv.append(productImage);
     
     var cardBody = $('<div>').addClass('card-body');
     cardDiv.append(cardBody);
@@ -38,34 +36,65 @@ $(document).ready(function(){
     
     var addToCartButton = $('<button>').addClass('btn btn-primary').text('Add to Cart').attr('id',index);
     addToCartButton.click(function(event){
-      console.log(event.target.id);
+      //console.log(event.target.id);
       var cartItem = products[event.target.id];
       cartItem.quantity = 1;
       cart.items.push(cartItem);
+      cart.total = cart.items.length * cartItem.price;
       $("#itemNo").text(cart.items.length);
+      $("#total").text(cart.total);
       localStorage.setItem('cart', JSON.stringify(cart)); //when add to cart button is clicked, saved to local storage, so when you refresh the data is still there
     });
     cardBody.append(addToCartButton);
+    
+    colDiv.append(cardDiv);
+    $('#products-row').append(colDiv);
   });
   
-  $("#itemNo").text(cart.items.length);
+  $("#itemNo").text(cart.items.length); //shows number of items in cart
+  $("#total").text(cart.total);
   
   cart.items.forEach(function(item, index){
     var colDiv = $('<div>').addClass('col-md-4');
-    $('#cart-row').append(colDiv);
-    
+  
     var cardDiv = $('<div>').addClass('card');//addClass and attr(Attribute) do the same thing
-    colDiv.append(cardDiv);
     
     var productImage = $('<img>').addClass("card-img-top");
-    cardDiv.append(productImage);
     productImage.attr('src', item.image);
+    cardDiv.append(productImage);
     
     var cardBody = $('<div>').addClass('card-body');
     cardDiv.append(cardBody);
     
     var productTitle = $('<h5>').addClass('card-title').text(item.name);
     cardBody.append(productTitle);
+    
+    var productPrice = $('<p>').addClass('card-text').text("$"+item.price + " x ");
+    cardBody.append(productPrice);
+    
+    var noOfProduct = $('<input type=number id=numOfProduct value=1 min=0>').bind('keyup mouseup', function(){
+      item.quantity = $('#numOfProduct').val();
+      //localStorage.setItem('cart', JSON.stringify(cart));
+    });
+    productPrice.append(noOfProduct);
+    
+    var addNewProduct =  $('<button>').addClass('btn btn-primary').text('Add').attr('id',index);
+    addNewProduct.click(function(event){
+      for (var i=0; i<item.quantity; i++){
+        var cartItem = products[event.target.id];
+        cart.items.push(cartItem); 
+      }
+      
+      cart.total = cart.total + (item.price * item.quantity);
+      
+      $('#itemNo').text(cart.items.length);
+      $('#total').text(cart.total);
+      localStorage.setItem('cart', JSON.stringify(cart));
+    });
+    cardBody.append(addNewProduct);
+    
+    colDiv.append(cardDiv);
+    $('#cart-row').append(colDiv);
   });
   
   $("#showCartBtn").click(function(){
